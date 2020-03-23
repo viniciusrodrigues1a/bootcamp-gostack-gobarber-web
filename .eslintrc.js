abbab720b3ade139c66aefadfe0aff3c20c83860
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-23 07:37:44 -0300

    :wrench: desabilitando rule jsx-props-no-spreading

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,6 +40,7 @@ module.exports = {
     camelcase: "off",
     "no-console": ["error", { allow: ["tron"] }],
     "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    "react-hooks/exhaustive-deps": "warn",
+    "react/jsx-props-no-spreading": 'off'
   },
 };
