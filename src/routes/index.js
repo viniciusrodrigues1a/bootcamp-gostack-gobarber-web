c5441ac2985f6acf0f1694cd44ad0d0953e23cb3
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-23 06:49:39 -0300

    :sparkles: criando as rotas

---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={SignIn} />
+      <Route exact path="/register" component={SignUp} />
+
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/profile" component={Profile} />
+    </Switch>
+  );
+}
